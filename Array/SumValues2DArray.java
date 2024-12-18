73573c9419619a9dd0703e284ea5b5c937f149f2
sanu <sanu.in397@gmail.com>
2024-12-18 14:07:46 +0530

    Refactor

--- Array/SumValues2DArray.java
+++ Array/SumValues2DArray.java
@@ -13,9 +13,10 @@
 class SumValues2DArray {
     public int maximumWealth(int[][] accounts) {
         int maxWealthSoFar = 0;
-        for (int[] customer accounts) {
+        for (int[] customer : accounts) {
             int currentCustomerWealth = 0;
-            for (int bank customer) {
+
+            for (int bank : customer) {
                 currentCustomerWealth += bank;
             }
             maxWealthSoFar = Math.max(maxWealthSoFar, currentCustomerWealth);
